7cec81c28e9a6d6f180a5cfa6826c70b509bcac1
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-09-28 09:17:06 +0200

    feat: add proper redix version to dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,21 @@ defmodule TdSe.Mixfile do
       {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:con_cache, "~> 0.13.0"},
+      {:cowboy, "~> 1.0"},
+      {:cabbage, git: "https://github.com/eriosv/cabbage.git"},
+      {:httpoison, "~> 1.0"},
+      {:edeliver, "~> 1.4.5"},
+      {:distillery, ">= 0.9.0", warn_missing: false},
+      {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false},
+      {:guardian, "~> 1.0"},
+      {:corsica, "~> 1.0"},
+      {:phoenix_swagger, "~> 0.7.0"},
+      {:redix, "0.7.1"},
+      {:ex_json_schema, "~> 0.5"},
+      {:json_diff, "~> 0.1.0"},
+      {:csv, "~> 2.0.0"},
+      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "v0.3.6"}
     ]
   end
 end
