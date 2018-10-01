e3f2f226327211812dfd4c07ca8167def25f832a
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-01 17:31:11 +0200

    feat: Add elasticsearch configuration in test environment

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,12 @@ config :td_se, TdSeWeb.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
+config :td_se, :elasticsearch,
+  search_service: TdSe.Search,
+  es_host: "localhost",
+  es_port: 9200,
+  type_name: "doc"
+
 config :td_se, permission_resolver: TdPerms.Permissions
 
 config :td_perms, redis_uri: "redis://localhost"
