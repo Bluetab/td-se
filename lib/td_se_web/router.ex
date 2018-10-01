e79b453c2eae08e7b5030fcf949fc16baa9637cc
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-01 17:45:11 +0200

    feat: Add endpoints to application's router

--- lib/td_se_web/router.ex
+++ lib/td_se_web/router.ex
@@ -1,6 +1,10 @@
 defmodule TdSeWeb.Router do
   use TdSeWeb, :router
 
+  @endpoint_url "#{Application.get_env(:td_se, TdLmWeb.Endpoint)[:url][:host]}:#{
+    Application.get_env(:td_se, TdLmWeb.Endpoint)[:url][:port]
+  }"
+
   pipeline :api do
     plug(TdSe.Auth.Pipeline.Unsecure)
     plug(:accepts, ["json"])
@@ -16,6 +20,13 @@ defmodule TdSeWeb.Router do
   end
 
   scope "/api", TdSeWeb do
-    pipe_through([:api, :api_secure, :api_authorized])
+    pipe_through([:api])
+    get "/ping", PingController, :ping
   end
+
+  scope "/api", TdSeWeb do
+    pipe_through([:api, :api_secure, :api_authorized])
+    post "/global_search", SearchController, :global_search
+  end
+
 end
