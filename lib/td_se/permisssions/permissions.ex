42247bd6b5fde463d5ae59ca95b15735553f1472
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-01 17:34:34 +0200

    feat: Add permissions context

---
+++ lib/td_se/permisssions/permissions.ex
@@ -0,0 +1,11 @@
+defmodule TdSe.Permissions do
+  @moduledoc """
+  The Permissions context.
+  """
+  alias TdSe.Accounts.User
+  @permission_resolver Application.get_env(:td_se, :permission_resolver)
+
+  def get_domain_permissions(%User{jti: jti}) do
+    @permission_resolver.get_acls_by_resource_type(jti, "domain")
+  end
+end
