2fd79423ce4689e441b2fd4c8a8e273abf3725e7
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-04 17:36:30 +0200

    chore: The api should run in server mode in prod

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :td_se, TdSeWeb.Endpoint,
   http: [port: 4006],
-  load_from_system_env: true,
   url: [host: "truedat.bluetab.net", port: 8005],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
