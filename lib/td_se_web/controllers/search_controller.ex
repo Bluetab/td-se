66143ce13931a2612c93f09957ce65cdfb6268d9
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-04 14:40:41 +0200

    feat: Render search results on controller

--- lib/td_se_web/controllers/search_controller.ex
+++ lib/td_se_web/controllers/search_controller.ex
@@ -2,16 +2,36 @@ defmodule TdSeWeb.SearchController do
   @moduledoc """
     Controller module for global search engine.
   """
+  use PhoenixSwagger
   use TdSeWeb, :controller
   alias TdSe.GlobalSearch
+  alias TdSeWeb.SearchResultsView
+  alias TdSeWeb.SwaggerDefinitions
 
   @all_indexes Application.get_env(:td_se, :elastic_indexes)
+
+  def swagger_definitions do
+    SwaggerDefinitions.global_search_definitions()
+  end
+
+  swagger_path :global_search do
+    post("/global_search")
+    description("Search for all of our indexes under the whole search space")
+    produces("application/json")
+    parameters do
+      search(
+        :body,
+        Schema.ref(:GlobalSearchRequest),
+        "Search query and filter parameters"
+      )
+    end
+    response(200, "OK", Schema.ref(:GlobalSearchResponse))
+  end
 
   def global_search(conn, params) do
     user = conn.assigns[:current_resource]
     params = add_indexes_to_params(params, Map.get(params, "indexes", nil))
-    %{results: results, total: _total} = do_search(user, params, 0, 10_000)
-    send_resp(conn, 200, results |> Poison.encode!)
+    user |> do_search(params, 0, 10_000) |> render_search_results(conn, params)
   end
 
   defp do_search(user, search_params, page, size) do
@@ -19,8 +39,8 @@ defmodule TdSeWeb.SearchController do
     size = search_params |> Map.get("size", size)
 
     search_params
-      |> Map.drop(["page", "size"])
-      |> GlobalSearch.search(user, page, size)
+    |> Map.drop(["page", "size"])
+    |> GlobalSearch.search(user, page, size)
   end
 
   defp add_indexes_to_params(params, nil) do
@@ -36,8 +56,30 @@ defmodule TdSeWeb.SearchController do
   defp build_params(params) do
     index_values =
       @all_indexes
-        |> Enum.map(fn {_k, v} -> v end)
+      |> Enum.map(fn {_k, v} -> v end)
 
     Map.put(params, "indexes", index_values)
+  end
+
+  defp render_search_results(%{results: results, total: total}, conn, %{"indexes" => indexes}) do
+    global_search_results =
+      indexes
+      |> Enum.uniq()
+      |> Enum.reduce([], fn index, acc ->
+        result_map =
+          %{}
+            |> Map.put("index", index)
+            |> Map.put("results", Enum.filter(results, &(&1["_index"] == index)))
+
+        acc ++ [result_map]
+      end)
+
+    conn
+    |> put_resp_header("x-total-count", "#{total}")
+    |> render(
+      SearchResultsView,
+      "global_search_results.json",
+      global_search_results: global_search_results
+    )
   end
 end
