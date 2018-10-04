b90ba2abc89380319598975944127062e1d56cfa
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-04 14:37:43 +0200

    feat: Add new controler view for result's search rendering

---
+++ lib/td_se_web/views/search_results_view.ex
@@ -0,0 +1,23 @@
+defmodule TdSeWeb.SearchResultsView do
+  use TdSeWeb, :view
+  alias TdSeWeb.SearchResultsView
+
+  def render("global_search_results.json", %{global_search_results: global_search_results}) do
+    %{data: render_many(global_search_results, SearchResultsView, "search_results.json")}
+  end
+
+  def render("search_results.json", %{search_results: search_results}) do
+    %{
+      index: Map.get(search_results, "index"),
+      results: render_many(Map.get(search_results, "results"), SearchResultsView, "search_result.json")
+    }
+  end
+
+  def render("search_result.json", %{search_results: search_results}) do
+    %{
+      id: Map.get(search_results, "id"),
+      name: Map.get(search_results, "name"),
+      index: Map.get(search_results, "_index")
+    }
+  end
+end
