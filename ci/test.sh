20ab283ee51035a1bafa1fed0717d5e2a62dde17
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-04 17:37:48 +0200

    feat: Add continuos integration config for build and test phases

---
+++ ci/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+redis-server --daemonize yes --protected-mode no
+service elasticsearch start
+
+cp -R /code /working_code
+cd /working_code
+
+echo "Starting test step"
+
+export MIX_ENV=test
+
+echo "Starting prebuild configuration"
+mix local.hex --force
+echo "local hex executed"
+mix local.rebar --force
+mix deps.clean --all
+echo "local rebar executed"
+echo "Downloading deps"
+mix deps.get
+echo "Starting tests"
+mix test || exit 1
+
+echo "Test step finish successfully"
