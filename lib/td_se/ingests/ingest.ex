e1833e67fb61f060839a0c07880bedbeaee18505
saul.isern <saul.isern@bluetab.net>
2019-02-19 14:20:28 +0100

    chore: [TD-1485] adapt global search engine to confidentiality verification and new index ingest

---
+++ lib/td_se/ingests/ingest.ex
@@ -0,0 +1,23 @@
+defmodule TdSe.Ingests.Ingest do
+  @moduledoc false
+
+  @status %{
+    draft: "draft",
+    pending_approval: "pending_approval",
+    rejected: "rejected",
+    published: "published",
+    versioned: "versioned",
+    deprecated: "deprecated"
+  }
+
+  def permissions_to_status do
+    %{
+      view_approval_pending_ingests: @status.pending_approval,
+      view_deprecated_ingests: @status.deprecated,
+      view_draft_ingests: @status.draft,
+      view_published_ingests: @status.published,
+      view_rejected_ingests: @status.rejected,
+      view_versioned_ingests: @status.versioned
+    }
+  end
+end
