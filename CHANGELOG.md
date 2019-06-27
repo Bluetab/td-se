80b0f6f8e0c4c4bf1047eac406a478a18fe0ec2e
Tom Crossland <tom.crossland@bluetab.net>
2019-06-27 16:58:38 +0200

    feat: [TD-1942] Jason instead of Poison for JSON encoding/decoding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## Unreleased
+
+## Changed
+
+- [TD-1681] Cache improvement (td-cache instead of td-perms)
+- [TD-1942] Use Jason instead of Posion for JSON encoding/decoding
+
 ## [3.0.0] 2019-06-25
 
 ### Changed
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,10 +1,14 @@
 defmodule TdSe.Factory do
   @moduledoc """
-   Factory builder for api entities
+  Factory builder for api entities
   """
+
+  alias Jason, as: JSON
+
   def build_user(%{"sub" => sub, "jti" => jti}) do
-    sub = sub |> Poison.decode!()
-    %TdSe.Accounts.User {
+    sub = sub |> JSON.decode!()
+
+    %TdSe.Accounts.User{
       id: Map.get(sub, "id", 0),
       user_name: Map.get(sub, "user_name", "invented_user"),
       is_admin: Map.get(sub, "is_admin", false),
