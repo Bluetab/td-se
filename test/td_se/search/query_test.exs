df42fc5f6dc88b50096693864964aafd3eae3a22
Guillermo RN <guillermo.r.navarrete@bluetab.net>
2023-06-29 15:31:05 +0000

    TD-5787 Add must param in elastic query for Boost option

--- test/td_se/search/query_test.exs
+++ test/td_se/search/query_test.exs
@@ -30,7 +30,7 @@ defmodule TdSe.Search.QueryTest do
                  should: [
                    %{
                      bool: %{
-                       filter: [
+                       must: [
                          %{term: %{"_index" => "concepts_idx"}},
                          %{term: %{"status" => "published"}}
                        ]
@@ -38,7 +38,7 @@ defmodule TdSe.Search.QueryTest do
                    },
                    %{
                      bool: %{
-                       filter: [
+                       must: [
                          %{term: %{"_index" => "ingests_idx"}},
                          %{term: %{"status" => "published"}}
                        ]
@@ -46,7 +46,7 @@ defmodule TdSe.Search.QueryTest do
                    },
                    %{
                      bool: %{
-                       filter: %{term: %{"_index" => "structures_idx"}},
+                       must: %{term: %{"_index" => "structures_idx"}},
                        must_not: %{exists: %{field: "deleted_at"}}
                      }
                    }
@@ -77,7 +77,7 @@ defmodule TdSe.Search.QueryTest do
       assert Query.build_query(%{"view_published_business_concepts" => :all}, @indices) ==
                %{
                  bool: %{
-                   filter: [
+                   must: [
                      %{term: %{"_index" => "concepts_idx"}},
                      %{term: %{"status" => "published"}}
                    ],
@@ -94,7 +94,7 @@ defmodule TdSe.Search.QueryTest do
              ) ==
                %{
                  bool: %{
-                   filter: [
+                   must: [
                      %{term: %{"_index" => "concepts_idx"}},
                      %{term: %{"status" => "published"}}
                    ]
@@ -103,7 +103,7 @@ defmodule TdSe.Search.QueryTest do
 
       assert Query.build_query(%{"view_published_business_concepts" => [1]}, @indices) == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{term: %{"domain_ids" => 1}},
                    %{term: %{"_index" => "concepts_idx"}},
                    %{term: %{"status" => "published"}}
@@ -120,7 +120,7 @@ defmodule TdSe.Search.QueryTest do
                @indices
              ) == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{term: %{"domain_ids" => 1}},
                    %{term: %{"_index" => "concepts_idx"}},
                    %{term: %{"status" => "published"}}
@@ -136,7 +136,7 @@ defmodule TdSe.Search.QueryTest do
                @indices
              ) == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{term: %{"domain_ids" => 1}},
                    %{
                      bool: %{
@@ -160,7 +160,7 @@ defmodule TdSe.Search.QueryTest do
       assert Query.build_query(%{"view_data_structure" => :all}, @indices) ==
                %{
                  bool: %{
-                   filter: %{term: %{"_index" => "structures_idx"}},
+                   must: %{term: %{"_index" => "structures_idx"}},
                    must_not: [
                      %{term: %{"confidential" => true}},
                      %{exists: %{field: "deleted_at"}}
@@ -174,14 +174,14 @@ defmodule TdSe.Search.QueryTest do
              ) ==
                %{
                  bool: %{
-                   filter: %{term: %{"_index" => "structures_idx"}},
+                   must: %{term: %{"_index" => "structures_idx"}},
                    must_not: %{exists: %{field: "deleted_at"}}
                  }
                }
 
       assert Query.build_query(%{"view_data_structure" => [1]}, @indices) == %{
                bool: %{
-                 filter: [%{term: %{"domain_ids" => 1}}, %{term: %{"_index" => "structures_idx"}}],
+                 must: [%{term: %{"domain_ids" => 1}}, %{term: %{"_index" => "structures_idx"}}],
                  must_not: [%{term: %{"confidential" => true}}, %{exists: %{field: "deleted_at"}}]
                }
              }
@@ -191,7 +191,7 @@ defmodule TdSe.Search.QueryTest do
                @indices
              ) == %{
                bool: %{
-                 filter: [%{term: %{"domain_ids" => 1}}, %{term: %{"_index" => "structures_idx"}}],
+                 must: [%{term: %{"domain_ids" => 1}}, %{term: %{"_index" => "structures_idx"}}],
                  must_not: %{exists: %{field: "deleted_at"}}
                }
              }
@@ -201,7 +201,7 @@ defmodule TdSe.Search.QueryTest do
                @indices
              ) == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{term: %{"domain_ids" => 1}},
                    %{
                      bool: %{
@@ -225,7 +225,7 @@ defmodule TdSe.Search.QueryTest do
       assert Query.build_query(%{"view_published_ingests" => :all}, @indices) ==
                %{
                  bool: %{
-                   filter: [
+                   must: [
                      %{term: %{"_index" => "ingests_idx"}},
                      %{term: %{"status" => "published"}}
                    ]
@@ -235,7 +235,7 @@ defmodule TdSe.Search.QueryTest do
       assert Query.build_query(%{"view_published_ingests" => [1]}, @indices) ==
                %{
                  bool: %{
-                   filter: [
+                   must: [
                      %{term: %{"domain_ids" => 1}},
                      %{term: %{"_index" => "ingests_idx"}},
                      %{term: %{"status" => "published"}}
@@ -261,7 +261,7 @@ defmodule TdSe.Search.QueryTest do
 
       assert concept_clause == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{term: %{"domain_ids" => 3}},
                    %{term: %{"_index" => "concepts_idx"}},
                    %{term: %{"status" => "published"}}
@@ -272,7 +272,7 @@ defmodule TdSe.Search.QueryTest do
 
       assert structure_clause == %{
                bool: %{
-                 filter: [
+                 must: [
                    %{
                      bool: %{
                        should: [
@@ -296,8 +296,16 @@ defmodule TdSe.Search.QueryTest do
 
       assert %{
                bool: %{
-                 filter: _,
-                 must: %{simple_query_string: %{query: "foo* bar*"}}
+                 must: [
+                   %{simple_query_string: %{query: "foo* bar*"}},
+                   %{term: %{"_index" => "ingests_idx"}},
+                   %{term: %{"status" => "published"}}
+                 ],
+                 should: [
+                   %{
+                     multi_match: %{operator: "and", query: "foo  bar*", type: "best_fields"}
+                   }
+                 ]
                }
              } = Query.build_query(permissions, @indices, query_string)
     end
