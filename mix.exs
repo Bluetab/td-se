bc169edb1a097d3d7a76f63b2c8016663a7c87e7
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-07-29 12:35:18 +0200

    chore: TD-6734 bump version 6.9.2

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule TdSe.Mixfile do
       app: :td_se,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "6.9.1-local"
+          nil -> "6.9.2-local"
           v -> v
         end,
       elixir: "~> 1.16",
